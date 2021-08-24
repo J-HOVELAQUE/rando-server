5dfc49d880346dc783ce27317b744b36c91089cc
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-08-24 17:31:28 +0200

    payload validation for create hike

--- dist/hike/controllers/recordHike.js
+++ dist/hike/controllers/recordHike.js
@@ -12,11 +12,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const HikeModel_1 = __importDefault(require("../model/HikeModel"));
+const joi_1 = __importDefault(require("joi"));
+const buildHikeRepository_1 = __importDefault(require("../repository/buildHikeRepository"));
+const hikeSchema = joi_1.default.object({
+    durationInMinutes: joi_1.default.number().required(),
+    elevationInMeters: joi_1.default.number().required(),
+    distanceInMeters: joi_1.default.number().required(),
+    startingAltitude: joi_1.default.number().required(),
+    arrivalAltitude: joi_1.default.number().required(),
+    description: joi_1.default.string(),
+    date: joi_1.default.date().required(),
+    participants: joi_1.default.array().items(joi_1.default.string()),
+    place: joi_1.default.string(),
+});
+const hikeRepository = buildHikeRepository_1.default();
 function default_1(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        const newHike = new HikeModel_1.default(req.body);
-        newHike.save();
+        const payload = req.body;
+        //// Payload validation
+        try {
+            joi_1.default.assert(payload, hikeSchema, {
+                abortEarly: false,
+            });
+        }
+        catch (error) {
+            const errorReport = error;
+            const errorMessages = errorReport.details.map((err) => err.message);
+            res.status(400).json({
+                error: "payloadError",
+                details: errorMessages,
+            });
+        }
         res.json({ message: "recorded" });
     });
 }
--- src/hike/controllers/recordHike.ts
+++ src/hike/controllers/recordHike.ts
@@ -1,10 +1,41 @@
 import HikeModel from "../model/HikeModel";
 import { Request, Response } from "express";
+import Joi, { ValidationError } from "joi";
+import buildHikeRepository from "../repository/buildHikeRepository";
+import Hike from "../../interfaces/hike";
+
+const hikeSchema = Joi.object({
+  durationInMinutes: Joi.number().required(),
+  elevationInMeters: Joi.number().required(),
+  distanceInMeters: Joi.number().required(),
+  startingAltitude: Joi.number().required(),
+  arrivalAltitude: Joi.number().required(),
+  description: Joi.string(),
+  date: Joi.date().required(),
+  participants: Joi.array().items(Joi.string()),
+  place: Joi.string(),
+});
+
+const hikeRepository = buildHikeRepository();
 
 export default async function (req: Request, res: Response) {
-  const newHike = new HikeModel(req.body);
+  const payload: Hike = req.body;
 
-  newHike.save();
+  //// Payload validation
+  try {
+    Joi.assert(payload, hikeSchema, {
+      abortEarly: false,
+    });
+  } catch (error) {
+    const errorReport: ValidationError = error;
+    const errorMessages: String[] = errorReport.details.map(
+      (err) => err.message
+    );
+    res.status(400).json({
+      error: "payloadError",
+      details: errorMessages,
+    });
+  }
 
   res.json({ message: "recorded" });
 }
