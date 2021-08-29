13b7f2432f65c87665e9abb6887d354c5c135d49
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-08-29 12:38:01 +0200

    var env for cloudinary

--- dist/services/uploadImage/uploadImageFromFileArray.js
+++ dist/services/uploadImage/uploadImageFromFileArray.js
@@ -15,47 +15,58 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cloudinary_1 = require("cloudinary");
 const jimp_1 = __importDefault(require("jimp"));
 const promises_1 = require("fs/promises");
+const config_1 = __importDefault(require("config"));
 const cloudinary = cloudinary_1.v2;
-cloudinary.config({
-    cloud_name: "dhov1sjr7",
-    api_key: "157842163261796",
-    api_secret: "MX3FORXNb-9duMrGfsI3RdJvvyg",
-    secure: true,
-});
 function uploadImageFromFileArray(files, placeName) {
     return __awaiter(this, void 0, void 0, function* () {
-        const fileArrayKeys = Object.keys(files);
-        if (Object.keys(files).length > 1) {
-            return {
-                outcome: "FAILURE",
-                errorCode: "INVALID_FILE",
-                detail: "Impossible to upload several files",
-            };
-        }
-        let pictureUrl = "";
-        for (const fileName in files) {
-            const newFile = files[fileName];
-            if (Array.isArray(newFile)) {
+        try {
+            if (Object.keys(files).length > 1) {
                 return {
                     outcome: "FAILURE",
                     errorCode: "INVALID_FILE",
                     detail: "Impossible to upload several files",
                 };
             }
-            const tempPicturePath = "./tmp/" + newFile.name;
-            const uploadedPicture = yield jimp_1.default.read(newFile.data);
-            uploadedPicture.resize(600, jimp_1.default.AUTO);
-            yield uploadedPicture.writeAsync(tempPicturePath);
-            const resultCloudinary = yield cloudinary.uploader.upload(`./tmp/${newFile.name}`, {
-                public_id: "rando/places/" + placeName,
-            });
-            pictureUrl = resultCloudinary.url;
-            promises_1.unlink(tempPicturePath);
+            let pictureUrl = "";
+            for (const fileName in files) {
+                const newFile = files[fileName];
+                if (Array.isArray(newFile)) {
+                    return {
+                        outcome: "FAILURE",
+                        errorCode: "INVALID_FILE",
+                        detail: "Impossible to upload several files",
+                    };
+                }
+                const tempPicturePath = "./tmp/" + newFile.name;
+                const uploadedPicture = yield jimp_1.default.read(newFile.data);
+                uploadedPicture.resize(600, jimp_1.default.AUTO);
+                yield uploadedPicture.writeAsync(tempPicturePath);
+                const cloudinaryConfig = {
+                    cloud_name: config_1.default.get("cloudinary.cloud_name"),
+                    api_key: config_1.default.get("cloudinary.api_key"),
+                    api_secret: config_1.default.get("cloudinary.api_secret"),
+                    secure: true,
+                };
+                cloudinary.config(cloudinaryConfig);
+                const resultCloudinary = yield cloudinary.uploader.upload(`./tmp/${newFile.name}`, {
+                    public_id: "rando/places/" + placeName,
+                });
+                pictureUrl = resultCloudinary.url;
+                promises_1.unlink(tempPicturePath);
+            }
+            return {
+                outcome: "SUCCESS",
+                data: pictureUrl,
+            };
         }
-        return {
-            outcome: "SUCCESS",
-            data: pictureUrl,
-        };
+        catch (error) {
+            console.log("EEERRRROR", error);
+            return {
+                outcome: "FAILURE",
+                errorCode: "CLOUDINARY_ERROR",
+                detail: error,
+            };
+        }
     });
 }
 exports.default = uploadImageFromFileArray;
--- src/services/uploadImage/uploadImageFromFileArray.ts
+++ src/services/uploadImage/uploadImageFromFileArray.ts
@@ -3,14 +3,16 @@ import { OutcomeSuccess, OutcomeFailure } from "../../interfaces/outcomes";
 import { v2 } from "cloudinary";
 import Jimp from "jimp";
 import { unlink } from "fs/promises";
+import config from "config";
 
 const cloudinary = v2;
-cloudinary.config({
-  cloud_name: "dhov1sjr7",
-  api_key: "157842163261796",
-  api_secret: "MX3FORXNb-9duMrGfsI3RdJvvyg",
-  secure: true,
-});
+
+interface CloudinaryConfig {
+  cloud_name: string;
+  api_key: string;
+  api_secret: string;
+  secure: boolean;
+}
 
 type UploadResult = OutcomeSuccess<string> | OutcomeFailure;
 
@@ -18,22 +20,8 @@ export default async function uploadImageFromFileArray(
   files: FileArray,
   placeName: string
 ): Promise<UploadResult> {
-  const fileArrayKeys = Object.keys(files);
-
-  if (Object.keys(files).length > 1) {
-    return {
-      outcome: "FAILURE",
-      errorCode: "INVALID_FILE",
-      detail: "Impossible to upload several files",
-    };
-  }
-
-  let pictureUrl: string = "";
-
-  for (const fileName in files) {
-    const newFile = files[fileName];
-
-    if (Array.isArray(newFile)) {
+  try {
+    if (Object.keys(files).length > 1) {
       return {
         outcome: "FAILURE",
         errorCode: "INVALID_FILE",
@@ -41,24 +29,54 @@ export default async function uploadImageFromFileArray(
       };
     }
 
-    const tempPicturePath: string = "./tmp/" + newFile.name;
+    let pictureUrl: string = "";
 
-    const uploadedPicture = await Jimp.read(newFile.data);
-    uploadedPicture.resize(600, Jimp.AUTO);
-    await uploadedPicture.writeAsync(tempPicturePath);
+    for (const fileName in files) {
+      const newFile = files[fileName];
 
-    const resultCloudinary = await cloudinary.uploader.upload(
-      `./tmp/${newFile.name}`,
-      {
-        public_id: "rando/places/" + placeName,
+      if (Array.isArray(newFile)) {
+        return {
+          outcome: "FAILURE",
+          errorCode: "INVALID_FILE",
+          detail: "Impossible to upload several files",
+        };
       }
-    );
-    pictureUrl = resultCloudinary.url;
-    unlink(tempPicturePath);
-  }
 
-  return {
-    outcome: "SUCCESS",
-    data: pictureUrl,
-  };
+      const tempPicturePath: string = "./tmp/" + newFile.name;
+
+      const uploadedPicture = await Jimp.read(newFile.data);
+      uploadedPicture.resize(600, Jimp.AUTO);
+      await uploadedPicture.writeAsync(tempPicturePath);
+
+      const cloudinaryConfig: CloudinaryConfig = {
+        cloud_name: config.get("cloudinary.cloud_name"),
+        api_key: config.get("cloudinary.api_key"),
+        api_secret: config.get("cloudinary.api_secret"),
+        secure: true,
+      };
+      cloudinary.config(cloudinaryConfig);
+
+      const resultCloudinary = await cloudinary.uploader.upload(
+        `./tmp/${newFile.name}`,
+        {
+          public_id: "rando/places/" + placeName,
+        }
+      );
+      pictureUrl = resultCloudinary.url;
+      unlink(tempPicturePath);
+    }
+
+    return {
+      outcome: "SUCCESS",
+      data: pictureUrl,
+    };
+  } catch (error) {
+    console.log("EEERRRROR", error);
+
+    return {
+      outcome: "FAILURE",
+      errorCode: "CLOUDINARY_ERROR",
+      detail: error,
+    };
+  }
 }
