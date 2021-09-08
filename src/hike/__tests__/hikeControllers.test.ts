6297e2bf8020dd4f2b2dea293da832c9dff4c9cd
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-09-08 09:06:28 +0200

    test for update hike

--- src/hike/__tests__/hikeControllers.test.ts
+++ src/hike/__tests__/hikeControllers.test.ts
@@ -360,4 +360,105 @@ describe("Testing hike controllers", () => {
       });
     });
   });
+
+  describe("PUT /hike/:hikeId", () => {
+    let hikeIdInDatabase: string;
+    let hikeObjectIdInDatabase: ObjectId;
+
+    beforeEach(async () => {
+      await HikeModel.deleteMany();
+      const hikeInDatabase = new HikeModel({
+        durationInMinutes: 120,
+        elevationInMeters: 900,
+        distanceInMeters: 6000,
+        startingAltitude: 1000,
+        arrivalAltitude: 1900,
+        description: "Très belle randonnée.",
+        date: "2021-12-03",
+        participants: [firstUserInDatabaseId, secondUserInDatabaseId],
+        place: placeInDatabaseId,
+      });
+
+      const saveResult = await hikeInDatabase.save();
+      hikeIdInDatabase = saveResult.id;
+      hikeObjectIdInDatabase = saveResult._id;
+    });
+
+    afterEach(async () => {
+      await HikeModel.deleteMany();
+    });
+
+    describe("Given that I try to update hike data but not participant and place", () => {
+      describe("When I PUT valid payload on /hike/:hikeId with other data", () => {
+        it("I receive success and hike data is updated", async () => {
+          const answer = await supertest(app)
+            .put("/hike/" + hikeIdInDatabase)
+            .send({
+              durationInMinutes: 180,
+              elevationInMeters: 1100,
+              distanceInMeters: 6500,
+              arrivalAltitude: 2100,
+              description: "Trop cool!!!!",
+              date: "2021-08-03",
+              participants: [secondUserInDatabaseId],
+            })
+            .set("Accept", "application/json")
+            .expect(200);
+
+          expect(answer.body).toEqual({
+            message: "update",
+            result: {
+              n: 1,
+              nModified: 1,
+              ok: 1,
+            },
+          });
+
+          const hikeData = await HikeModel.findById(hikeIdInDatabase);
+          expect(hikeData).not.toBeNull();
+
+          if (hikeData) {
+            expect(hikeData.durationInMinutes).toBe(180);
+            expect(hikeData.elevationInMeters).toBe(1100);
+            expect(hikeData.distanceInMeters).toBe(6500);
+            expect(hikeData.arrivalAltitude).toBe(2100);
+            expect(hikeData.description).toBe("Trop cool!!!!");
+            expect(hikeData.date).toEqual(new Date("2021-08-03"));
+          }
+        });
+      });
+    });
+
+    describe("Given that I try to update a place with an invalid payload", () => {
+      describe("When I PUT an invalid payload on /hike/:hikeId", () => {
+        it("Then I receive a faiure and the hike isn't updated", async () => {
+          const answer = await supertest(app)
+            .put("/hike/" + hikeIdInDatabase)
+            .send({
+              durationInMinutes: "Cent quatre vingt",
+              date: "Une Date",
+            })
+            .set("Accept", "application/json")
+            .expect(400);
+
+          expect(answer.body).toEqual({
+            details: [
+              '"durationInMinutes" must be a number',
+              '"date" must be a valid date',
+            ],
+            error: "payloadError",
+          });
+
+          const hikeData = await HikeModel.findById(hikeIdInDatabase);
+
+          expect(hikeData).not.toBeNull();
+
+          if (hikeData) {
+            expect(hikeData.durationInMinutes).toBe(120);
+            expect(hikeData.date).toEqual(new Date("2021-12-03"));
+          }
+        });
+      });
+    });
+  });
 });
