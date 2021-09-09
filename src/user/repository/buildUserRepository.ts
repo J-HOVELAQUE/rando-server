8263c0f1493cdbc10c90d50b5c45808428347847
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-09-09 16:41:41 +0200

    method update on user repository

--- src/user/repository/buildUserRepository.ts
+++ src/user/repository/buildUserRepository.ts
@@ -2,11 +2,35 @@ import UserModel from "../model/UserModel";
 import User from "../../interfaces/user";
 import { OutcomeSuccess, OutcomeFailure } from "../../interfaces/outcomes";
 
-type ResultRepoMethod<data> = OutcomeSuccess<data> | OutcomeFailure;
+type UserRepositoryError =
+  | "DATABASE_ERROR"
+  | "UNIQUE_CONSTRAIN_ERROR"
+  | "ID_NOT_FOUND"
+  | "CAST_ERROR"
+  | "RELATIONAL_ERROR";
+
+interface UserRepositoryOutcomeFailure extends OutcomeFailure {
+  errorCode: UserRepositoryError;
+}
+
+type ResultRepoMethod<data> =
+  | OutcomeSuccess<data>
+  | UserRepositoryOutcomeFailure;
+
+interface UserDataToUpdate {
+  name?: String;
+  firstname?: String;
+  email?: string;
+  dateOfBirth?: Date;
+}
 
 interface UserRepository {
   create: (userToCreate: User) => Promise<ResultRepoMethod<User>>;
   findAll: () => Promise<ResultRepoMethod<User[]>>;
+  update: (
+    userId: string,
+    userData: UserDataToUpdate
+  ) => Promise<ResultRepoMethod<any>>;
 }
 
 export default function buildUserRepository(): UserRepository {
@@ -19,14 +43,14 @@ export default function buildUserRepository(): UserRepository {
           outcome: "SUCCESS",
           data: dbAnswer,
         };
-      } catch (error) {
+      } catch (error: any) {
         let errorCode: string = "DATABASE_ERROR";
         if (error.code && error.code === 11000) {
           errorCode = "UNIQUE_CONSTRAIN_ERROR";
         }
         return {
           outcome: "FAILURE",
-          errorCode: errorCode,
+          errorCode: "DATABASE_ERROR",
           detail: error,
         };
       }
@@ -39,7 +63,31 @@ export default function buildUserRepository(): UserRepository {
           outcome: "SUCCESS",
           data: userInDatabase,
         };
-      } catch (error) {
+      } catch (error: any) {
+        return {
+          outcome: "FAILURE",
+          errorCode: "DATABASE_ERROR",
+          detail: error,
+        };
+      }
+    },
+
+    update: async (userId: string, userData: UserDataToUpdate) => {
+      try {
+        const result = await UserModel.updateOne({ _id: userId }, userData);
+
+        return {
+          outcome: "SUCCESS",
+          data: result,
+        };
+      } catch (error: any) {
+        if (error.name === "CastError") {
+          return {
+            outcome: "FAILURE",
+            errorCode: "CAST_ERROR",
+            detail: error,
+          };
+        }
         return {
           outcome: "FAILURE",
           errorCode: "DATABASE_ERROR",
