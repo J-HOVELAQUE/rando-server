24a64b4f9a3fd4be9f9ffc3b20ad34d6b308de57
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-09-12 11:28:03 +0200

    mapping editPlacePicture on route /place/:placeId/picture (PUT)

--- src/place/router/index.ts
+++ src/place/router/index.ts
@@ -3,6 +3,7 @@ import recordPlace from "../controllers/recordPlace";
 import getPlace from "../controllers/getPlace";
 import editPlaceData from "../controllers/editPlace";
 import deletePlace from "../controllers/deletePlace";
+import editPlacePicture from "../controllers/editPlacePicture";
 
 const placeRouter = express.Router();
 
@@ -11,6 +12,8 @@ placeRouter.get("/", getPlace);
 placeRouter.post("/", recordPlace);
 
 placeRouter.put("/:placeId", editPlaceData);
+
+placeRouter.put("/:placeId/picture", editPlacePicture);
 
 placeRouter.delete("/:placeId", deletePlace);
 
