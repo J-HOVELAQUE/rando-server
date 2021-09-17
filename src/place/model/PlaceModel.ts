42c0c969a6e48617920c5fd6275b7cf084c864a3
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-09-17 10:07:12 +0200

    adding location on place schema

--- src/place/model/PlaceModel.ts
+++ src/place/model/PlaceModel.ts
@@ -7,6 +7,17 @@ const placeSchema = new Schema<Place>({
   altitudeInMeters: { type: Number, required: true },
   city: String,
   picture: String,
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
 });
 
 const PlaceModel = model<Place>("Places", placeSchema);
